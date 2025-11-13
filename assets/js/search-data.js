78c1231063c8ad6638e84b855dc90379dae4cb91
RaghuHemadri <45795135+RaghuHemadri@users.noreply.github.com>
2025-11-13 14:34:46 +0000

    Deploying to gh-pages from @ RaghuHemadri/raghuhemadri.github.io@a6fec507e43d06edf67d8ab5bb208ee918eca0c5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,7 +44,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-ml-sys-design-youtube-recommendation-system",
+        },{id: "post-ml-sys-design-decoding-the-app-store-recommendation-system",
+      
+        title: "ML Sys Design: Decoding the App Store Recommendation System",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/app-store-reco/";
+        
+      },
+    },{id: "post-ml-sys-design-youtube-recommendation-system",
       
         title: "ML Sys Design: YouTube Recommendation System",
       
